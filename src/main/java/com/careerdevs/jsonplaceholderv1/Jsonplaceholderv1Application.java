84a7dea3acd00477147c112bda9afdb4ba0a2300
Richies Luong <richiesluong.student@careerdevs.com>
2021-11-27 10:37:02 -0500

    Intial commit for Json Placeholder API v1

---
+++ src/main/java/com/careerdevs/jsonplaceholderv1/Jsonplaceholderv1Application.java
@@ -0,0 +1,13 @@
+package com.careerdevs.jsonplaceholderv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jsonplaceholderv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jsonplaceholderv1Application.class, args);
+	}
+
+}

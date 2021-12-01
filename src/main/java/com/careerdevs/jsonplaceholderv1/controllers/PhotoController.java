e34838093e8f0ef8894b7f8b18d7503802753376
Richies Luong <richiesluong.student@careerdevs.com>
2021-12-01 16:10:58 -0500

    userRange methods works

--- src/main/java/com/careerdevs/jsonplaceholderv1/controllers/PhotoController.java
+++ src/main/java/com/careerdevs/jsonplaceholderv1/controllers/PhotoController.java
@@ -1,21 +1,25 @@
 package com.careerdevs.jsonplaceholderv1.controllers;
 
+import com.careerdevs.jsonplaceholderv1.models.User;
 import com.careerdevs.jsonplaceholderv1.models.UserPhotos;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
+import java.util.ArrayList;
+import java.util.Collections;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/photos")
 public class PhotoController {
 
-    @GetMapping("/photos")
+    @GetMapping("/")
     public Object getPhotos(RestTemplate restTemplate) {
         String URL = UserController.JPURL + "photos";
         return restTemplate.getForObject(URL, UserPhotos[].class);
     }
 
-    @GetMapping("/photos/{id}")
+    @GetMapping("/{id}")
     public Object getPhotoID(RestTemplate restTemplate,
                              @PathVariable String id) {
         String URL = UserController.JPURL + "photos/" + id;
@@ -33,12 +37,88 @@ public class PhotoController {
         }
     }
 
-//    @GetMapping("/photos")
-//    public Object userRange(RestTemplate restTemplate,
-//                            @RequestParam(name = "id") String start,
-//                            @RequestParam(name = "id") String end) {
-//        String URL = UserController.JPURL + "users" + start + end;
-//        return restTemplate.getForObject(URL, UserPhotos[].class);
-//    }
+
+    @GetMapping("/{start}/{end}")
+    public Object photoRange(RestTemplate restTemplate,
+                            @PathVariable(name = "start") String start, @PathVariable(name = "end") String end) {
+
+        try {
+            ArrayList<UserPhotos> allPhotos = new ArrayList<>();
+
+            int startId = Integer.parseInt(start), endId = Integer.parseInt(end);
+
+            for (int i = startId; i <= endId; i++) {
+
+                String URL = UserController.JPURL + "photos/" + i;
+                UserPhotos photos = restTemplate.getForObject(URL, UserPhotos.class);
+                Collections.addAll(allPhotos, photos);
+
+            }
+            return allPhotos;
+
+        } catch (HttpStatusCodeException e) {
+
+            return "ID's out of range, please enter an ID between " + start + " and " + end;
+
+        } catch (Exception e) {
+
+            return "ID's do not match";
+        }
+    }
+
+
+    // Post Method
+    @PostMapping("/post")
+    public UserPhotos postPhoto(RestTemplate restTemplate, @RequestBody User user) {
+
+        String URL = UserController.JPURL + "photos/";
+        return restTemplate.postForObject(URL, user, UserPhotos.class);
+    }
+
+    // Put Method
+    @PutMapping("/put/{id}")
+    public Object putPhoto(RestTemplate restTemplate, @PathVariable String id,
+                           @RequestBody UserPhotos userPhotos) {
+
+        String URL = UserController.JPURL + "photos/" + id;
+        try {
+
+            restTemplate.put(URL, userPhotos);
+
+            return "Successfully updated user: " + id;
+
+        } catch (HttpClientErrorException.NotFound e) {
+
+            return "ID not found";
+
+        } catch (Exception e) {
+
+            System.out.println(e.getMessage());
+            return e.getMessage();
+        }
+    }
+
+
+    // Delete Method
+    @DeleteMapping("/delete/{id}")
+    public String deletePhoto(RestTemplate restTemplate, @PathVariable String id) {
+
+        String URL = UserController.JPURL + "photos/" + id;
+
+        try {
+            restTemplate.delete(URL);
+
+            return "Successfully deleted a user: " + id;
+
+        } catch (HttpClientErrorException.NotFound e) {
+
+            return "ID not found";
+
+        } catch (Exception e) {
+
+            System.out.println(e.getMessage());
+            return e.getMessage();
+        }
+    }
 
 }
